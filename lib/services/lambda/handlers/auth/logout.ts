dca82d9945ddde6a04e79a88f8d54fb1d22476e1
cal macconnachie <cal.macconnachie@gmail.com>
2025-08-20 20:25:00 -0700

    Revert "fix: use auth header for logout"
    This reverts commit 02268cf6f57a596cd20c00214e6aeaa56367bdf8.

--- lib/services/lambda/handlers/auth/logout.ts
+++ lib/services/lambda/handlers/auth/logout.ts
@@ -6,28 +6,16 @@ import { APIGatewayProxyEvent } from 'aws-lambda'
 const cognitoClient = new CognitoIdentityProviderClient({})
 
 export const logout = async (event: APIGatewayProxyEvent) => {
-  const authHeader = event.headers?.Authorization || event.headers?.authorization
-  let accessToken = ''
-  if (authHeader && authHeader.startsWith('Bearer ')) {
-    accessToken = authHeader.slice(7)
-  }
-  if (!accessToken) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Missing or invalid Authorization header' }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Content-Type': 'application/json'
-      }
-    }
-  }
+  const { body } = event
+  const { accessToken } = JSON.parse(body || '{}')
 
   try {
     const command = new GlobalSignOutCommand({
       AccessToken: accessToken
     })
+
     await cognitoClient.send(command)
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Logout successful' }),
