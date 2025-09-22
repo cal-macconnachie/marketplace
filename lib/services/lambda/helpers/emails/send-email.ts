b524cc4f720bedb2e3d53661c89c298571de9bbc
cal macconnachie <cal.macconnachie@gmail.com>
2025-09-21 21:09:13 -0700

    fix: add debug log

--- lib/services/lambda/helpers/emails/send-email.ts
+++ lib/services/lambda/helpers/emails/send-email.ts
@@ -63,6 +63,15 @@ export async function sendEmail(params: SendEmailParams): Promise<void> {
     subject: params.subject,
     body: params.body,
   }
+  console.log({
+    to: params.to,
+    cc: params.cc,
+    bcc: params.bcc,
+    reply_to: params.reply_to,
+    from: params.from,
+    subject: params.subject,
+    body: params.body.length
+  })
 
   const command = new InvokeCommand({
     FunctionName: targetArn!, // full ARN for cross-account invocation
