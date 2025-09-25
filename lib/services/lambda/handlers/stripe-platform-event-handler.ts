5137ee67cdee38f15b3433fb53fd7968bec0c83c
cal macconnachie <cal.macconnachie@gmail.com>
2025-09-24 19:56:00 -0700

    fix: use stripe customer id index

--- lib/services/lambda/handlers/stripe-platform-event-handler.ts
+++ lib/services/lambda/handlers/stripe-platform-event-handler.ts
@@ -227,10 +227,15 @@ export const stripePlatformEventHandler = async (event: EventBridgeEvent<'Stripe
         return
       }
 
-      const user = await get<User>({
+      const users = await queryAll<User>({
         tableName: process.env.USERS_TABLE!,
-        key: { stripeUserId: customerId }
+        indexName: 'stripe_id-index',
+        keyConditionExpression: 'stripe_id = :stripeId',
+        expressionAttributeValues: {
+          ':stripeId': customerId
+        }
       })
+      const user = users[0]
       
       if (!user) {
         console.error('No user found for stripeUserId:', customerId)
