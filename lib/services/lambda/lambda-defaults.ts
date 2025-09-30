ee0b27a24fd595f3cb84c34a656b4d478e53c3f9
cal macconnachie <cal.macconnachie@gmail.com>
2025-09-30 11:12:58 -0700

    fix: source map support external?

--- lib/services/lambda/lambda-defaults.ts
+++ lib/services/lambda/lambda-defaults.ts
@@ -19,6 +19,7 @@ export function createDefaultNodejsFunction(scope: Construct, id: string, props:
     bundling: {
       sourceMap: true,
       banner: "require('source-map-support').install();",
+      nodeModules: ['source-map-support'],
       ...props.bundling
     },
     ...props
@@ -50,6 +51,7 @@ export function createNodejsFunctionWithNativeDeps(
     bundling: {
       sourceMap: true,
       banner: "require('source-map-support').install();",
+      nodeModules: ['source-map-support'],
       ...nativeBundling
     },
     ...functionProps
