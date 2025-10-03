fc4b05660eb0f117cdf1be618a0a3c2d6448c47d
cal macconnachie <cal.macconnachie@gmail.com>
2025-10-03 12:57:34 -0700

    fix: trigger deploy

--- lib/services/lambda/handlers/auth/post-auth-trigger.ts
+++ lib/services/lambda/handlers/auth/post-auth-trigger.ts
@@ -5,8 +5,6 @@ import { User } from '../users'
 import { getUserByEmail } from '../../helpers/users/get-user-by-email'
 
 export const postAuthTrigger = async (event: PostAuthenticationTriggerEvent) => {
-  console.log('Post Authentication Trigger Event:', JSON.stringify(event))
-  
   const { userAttributes } = event.request
   const email = userAttributes.email
   const cognitoId = userAttributes.sub
