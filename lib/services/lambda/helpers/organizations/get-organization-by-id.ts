086d1c55916b577a58acd74105c02a29c58f4ab1
cal macconnachie <cal.macconnachie@gmail.com>
2025-10-06 12:31:35 -0700

    fix: remove duplicate stuff

--- lib/services/lambda/helpers/organizations/get-organization-by-id.ts
+++
@@ -1,9 +0,0 @@
-import { get } from '../dynamo-helpers/get'
-import { Organization } from '../../handlers/organizations'
-
-export const getOrganizationById = async (id: string): Promise<Organization | undefined> => {
-  return get<Organization>({
-    tableName: process.env.ORGANIZATIONS_TABLE!,
-    key: { id }
-  })
-}
