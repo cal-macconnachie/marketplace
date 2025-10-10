102e9662a88a62ba048e95fe6b4c937de4aad0f7
cal macconnachie <cal.macconnachie@gmail.com>
2025-10-09 21:18:23 -0700

    fix: ts compile

--- backend/bin/marketplace-api-deployment.ts
+++ backend/bin/marketplace-api-deployment.ts
@@ -5,13 +5,6 @@ import { MarketplaceApiDeploymentStack } from '../lib/marketplace-api-deployment
 const app = new cdk.App()
 const envName = app.node.tryGetContext('envName') ?? 'dev'
 
-const env = {
-  account: process.env.CDK_DEFAULT_ACCOUNT || process.env.AWS_ACCOUNT_ID,
-  region: process.env.CDK_DEFAULT_REGION || 'us-east-1'
-}
-
 new MarketplaceApiDeploymentStack(app, `MarketplaceApiDeployment-${envName}`, {
-  envName,
-  env
+  envName
 })
-
